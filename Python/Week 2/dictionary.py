e2f758cfe5c6531726bc6229bcf19925fe2deb72
Linus Msuri <razer5854@gmail.com>
2025-04-02 17:32:01 +0300

    Update dictionary.py

--- Python/Week 2/dictionary.py
+++ Python/Week 2/dictionary.py
@@ -1,2 +1,23 @@
-student_ids = {1, 2, 3, 4, 5}
-print(student_ids)
+students = {
+    'name': 'John',
+    'age': 25,
+    'gender': 'male',
+    'is_student': True,
+    'courses': ['Math', 'Science', 'History'],
+    
+    'my_dict': {
+        'key1': 'value1',
+        'key2': 'value2'
+    }
+}
+
+print(students)
+
+print(students['name'])
+
+students['age'] = 26
+print(students)
+
+students['height'] = 5.8
+print(students)
+print(students['height'])
