bc0059b30b7aa30aa53865bbf8168a92652343e1
Linus Msuri <razer5854@gmail.com>
2025-04-02 17:28:52 +0300

    Week 1 Activity

---
+++ Python/Week 1/index.py
@@ -0,0 +1,18 @@
+print("Hello, world")
+
+number = 10
+print(number)
+
+age = 23 # Int DataType
+marks = 99.5 # Float DataType
+name = "John Doe" # String DataType
+
+# Lists DataTypes
+languages = ["Python", "HTML", "CSS", 3, 5.7, True]
+print(languages)
+print(languages[2])
+print(languages[-4])
+
+print(languages[1:4])
+print(languages[:3])
+print(languages[2:])
